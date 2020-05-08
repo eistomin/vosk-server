d93814f7933fb7c5c27b7f96dcf2d8afe436ad42
Eugene Istomin <eugene.istomin@gmail.com>
2020-05-08 07:16:32 +0300

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -3,10 +3,10 @@
 set -e
 set -x
 
-for kind in vosk-server vosk-server-atom ru en de cn en-atom fr-atom ru-atom ; do
-    docker build --squash --file Dockerfile.kaldi-${kind} --tag alphacep/kaldi-${kind}:latest .
+for kind in vosk-server ru en; do
+    docker build --squash --file Dockerfile.kaldi-${kind} --tag contextme/kaldi-${kind}:latest .
 done
 
-for kind in vosk-server vosk-server-atom ru en de cn en-atom fr-atom ru-atom ; do
-    docker push alphacep/kaldi-${kind}:latest
+for kind in vosk-server ru en; do
+    docker push contextme/kaldi-${kind}:latest
 done
